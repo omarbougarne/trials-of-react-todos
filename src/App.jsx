d30400a1753814964d760fe6b883f985200f8c20
omarbougarne <omarbougarne98@gmail.com>
2025-08-27 18:24:42 +0100

    side-effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Input  from './components/elements/input'
 import Button  from './components/elements/button'
 import List  from './components/elements/List'
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos')
+    return savedTodos ? JSON.parse(savedTodos) : []
+  })
   const [inputValue, setInputValue] = useState('')
   
+
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
   const addTodo = () => {
-    setTodos([...todos, {id: Date.now(), text: inputValue}])
+    setTodos([...todos, {id: Date.now(), text: inputValue, completed: false}])
   }
 
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo ))
   }
   return (
     <>
@@ -23,7 +35,7 @@ function App() {
           <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
           <Button text="Add" onClick={addTodo} type="primary"/>
         </div>
-          <List items={todos} onDelete={deleteTodo}/>
+          <List items={todos} onDelete={deleteTodo} onToggle={toggleTodo}/>
     </div>
       
     </>
--- src/components/elements/List.jsx
+++ src/components/elements/List.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function List({ items, onDelete }) {
+
+export default function List({ items, onDelete, onToggle }) {
   return (
-    <ul>
+    <ul className="todo-list">
       {items.map((item, index) => (
-        <li key={index}>
-          {item.text}
+        <li key={index} className={item.completed ? 'completed' : ''}>
+          <input
+            type="checkbox"
+            checked={item.completed}
+            onChange={() => onToggle(item.id)}
+          />
+          <span className="todo-text">{item.text}</span>
           <button onClick={() => onDelete(item.id)}>Delete</button>
         </li>
       ))}
